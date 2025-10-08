d22934744f445eb2bf0f3d99aedbb8e8d72d1616
tearupheyfish <desolationsong@163.com>
2025-10-08 08:01:03 +0800

    分离不同用途的main()

--- src_gui/src/main.cpp
+++ src_gui/src/main.cpp
@@ -13,13 +13,6 @@
 #include <QFile>
 #include <memory>
 
-void prtEnvInfo()
-{
-    qInfo()<<"====环境信息========================";
-    qInfo()<< PrintProcessGroups();
-    qInfo()<< "版本: "<<VERSION_STRING;
-}
-
 //@ref DevDoc/logging.cov.md
 void customLogHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
 #ifndef USE_LOG
@@ -81,87 +74,79 @@ void customLogHandler(QtMsgType type, const QMessageLogContext &context, const Q
         << '\n';
 }
 
+int main_update(int, char **);
+int main_setter(int, char **);
 
-FpsSetter initialSetter()
-{
-    HWND targetWindow = FindWindowW(nullptr, L"第五人格");
-    if (!targetWindow)
-    {
-        qCritical()<<"未找到游戏窗口";
-        ErrorReporter::instance()->receive({ErrorReporter::严重,"未找到第五人格窗口"});
-        return {};
-    }
-
-    DWORD pid;
-    GetWindowThreadProcessId(targetWindow, &pid);
-
-    return {pid};
-}
-
+#ifndef GUI_BUILD_SINGLE
+#define main_update sizeof
+#endif
 int main(int argc, char *argv[])
 {
-#ifdef GUI_BUILD_SINGLE
-    if (argc > 1)
+    switch (argc)
     {
-        std::filesystem::path newExePath = argv[1];
-        std::filesystem::path curDir = argv[2];
-        if (argc > 3)
-        {
-            int64_t waitpid = std::stoull(argv[3]);
-            HANDLE hProcess = OpenProcess(SYNCHRONIZE, FALSE, waitpid);
-            if (!hProcess)
-            {
-                std::cerr<<"出错：未能访问指定进程";
-                return 1;
-            }
-            //Windows中对象本身就是可等待的
-            WaitForSingleObject(hProcess, INFINITE);
-
-            CloseHandle(hProcess);
-        }
-        auto to = curDir/newExePath.filename();
-        try
-        {
-            std::filesystem::copy(newExePath, to, std::filesystem::copy_options::overwrite_existing);
-        }catch (std::exception&e)
-        {
-            qCritical()<<e.what();
-            return -1;
-        }
-        return 0;
+        case 1 : return main_setter(argc, argv);
+        default: return main_update(argc, argv);
     }
-#endif
+}
+#undef main_update
+
+int main_setter(int argc, char *argv[])
+{
     qInstallMessageHandler(customLogHandler);
-    prtEnvInfo();
 
-    QApplication a(argc, argv);
-    a.setApplicationVersion(VERSION_STRING);
+    qInfo()<<"====环境信息========================";
+    qInfo()<< "版本: "<<VERSION_STRING;
+    qInfo()<< PrintProcessGroups();
 
-    FpsSetter setter;
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication app(argc, argv);
+    QApplication::setApplicationVersion(VERSION_STRING);
+    QApplication::setQuitOnLastWindowClosed(false);//手动管理
 
-    FpsDialog w; //需要再fpssetter后创建，才不会让timer访问已经销毁的setter
-    w.setWindowTitle("第五帧率解锁");
-    w.setFixedSize(w.width(), w.height());
 
+    std::unique_ptr<FpsDialog> w {FpsDialog::create()};
+    w->show();
 
     UpdateDialog ifm;
     UpdateChecker uc(ifm);
     ifm.setRelativeData(uc);
+    ifm.hide();
 
-    AppLifeManager lifemgr(a, uc, w, ifm, setter);
-    /// lifemgr需要setter的实例，因此lifemgr必须在setter之后初始化
-    /// setter的初始化报错了无法给予lifemgr的槽函数，因此setter必须在lifemgr之后初始化
-
-    setter = initialSetter();
-    if(setter)
-    {
-        w.setRelativedData(setter);
-        w.show();
-    }
+    AppLifeManager lifemgr(app, uc, *w, ifm);
+    // setter的初始化报错时lifemgr需要已经注册信号槽，因此setter必须在lifemgr之后初始化
 
     uc.checkUpdate();
 
     qInfo()<<"====运行========================";
-    return a.exec();
+    return app.exec();
+}
+
+[[maybe_unused]]
+int main_update(int argc, char *argv[])
+{
+    std::filesystem::path newExePath = argv[1];
+    std::filesystem::path curDir = argv[2];
+    if (argc > 3)
+    {
+        int64_t waitpid = std::stoull(argv[3]);
+        HANDLE hProcess = OpenProcess(SYNCHRONIZE, FALSE, waitpid);
+        if (!hProcess)
+        {
+            std::cerr<<"出错：未能访问指定进程";
+            return 1;
+        }
+        //Windows中对象本身就是可等待的
+        WaitForSingleObject(hProcess, INFINITE);
+
+        CloseHandle(hProcess);
+    }
+    auto to = curDir/newExePath.filename();
+    try
+    {
+        std::filesystem::copy(newExePath, to, std::filesystem::copy_options::overwrite_existing);
+    }catch (std::exception&e)
+    {
+        qCritical()<<e.what();
+        return -1;
+    }
+    return 0;
 }
